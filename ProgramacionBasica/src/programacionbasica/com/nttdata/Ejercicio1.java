d60ef55912b6fa3cec8ffebfe0146bde19cd6936
hscarinci <herdiesca@gmail.com>
2022-08-05 09:56:33 -0300

    Se agrega el enunciado de Ejerecicio 1

--- ProgramacionBasica/src/programacionbasica/com/nttdata/Ejercicio1.java
+++ ProgramacionBasica/src/programacionbasica/com/nttdata/Ejercicio1.java
@@ -3,6 +3,7 @@ package programacionbasica.com.nttdata;
 
 
 public class Ejercicio1 {
+    /*DECLARAR LA VARIABLE “DIVISION1” CUYO VALOR SEA IGUAL ALA DIVISIÓN ENTRE LA VARIABLE “C” Y “B” RESPECTIVAMENTE;Y VALIDAR SI EL RESULTADO ES UN NUMERO POSITIVO,NEGATIVO, IGUAL A CERO O DIVISIÓN NO POSIBLE.*/
     public static void main(String[] args) {
         //Ejercicio 1
         System.out.println("Bienvenidos al mundo Java!");
